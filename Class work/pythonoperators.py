41823866e2faeea3edd2be408f46adcb9dc9d2d0
Abhinay5122 <naniamaravarapu5122@gmail.com>
2025-07-08 14:39:04 +0530

    folders created

---
+++ Class work/pythonoperators.py
@@ -0,0 +1,23 @@
+#4. python operators.py
+
+#1. arithmetic Operators
+a=20
+b=30
+print('addition (a+b):',a+b) # addition (a+b): 50
+print('subraction(a-b):',a-b) #subraction(a-b): -10
+print('multiplication(a*b):',a*b)# multiplication(a*b): 600
+print('divsion(a/b):',a/b)# divsion(a/b): 0.6666666666666666
+print('floor divsion(a//b:',a//b)#floor divsion(a//b: 0
+print('modulus(a%b):',a%b)#modulus(a%b): 20
+print('exponentiation(a**b);',a**b)# exponentiation(a**b); 1073741824000000000000000000000000000000
+
+
+# 2. comparision operators
+c=56
+d=23
+print('equal to (c==d):',c==d)#equal to (c==d): False
+print('not equal to(c!=d):',c!=d)#not equal to(c!=d): True
+print('greater than(c>d):',c>d)#greater than(c>d): True
+print('less than(c<d):',c<d)#ess than(c<d): False
+print('greater than or equal to(c>=d):',c>=d)#greater than or equal to(c>=d): True
+print('less than or equal to(c<=d):',c<=d)#less than or equal to(c<=d): False
